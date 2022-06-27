a4f6ff0b947674bae525c53cd1ab87e214ba720b
Francisco Nardi <francisnardi@gmail.com>
2022-06-27 12:55:57 -0300

    user

--- 02_2808_ef_sql/03_fluent_ef_crud/Program.cs
+++ 02_2808_ef_sql/03_fluent_ef_crud/Program.cs
@@ -1,4 +1,7 @@
-﻿using Blog.Data;
+﻿using System;
+using System.Linq;
+using Blog.Data;
+using Blog.Models;
 
 namespace Blog
 {
@@ -7,6 +10,32 @@ namespace Blog
         static void Main(string[] args)
         {
             using var context = new BlogDataContext();
+
+            User fran = new User
+            {
+                Name = "Fran",
+                Bio = "CS Developer",
+                Email = "fjnf@fjnf.com",
+                Image = "blob",
+                PasswordHash = "Hash",
+                Slug = "fran"
+            };
+            var post = new Post
+            {
+                Author = fran,
+                Body = "Meu artigo",
+                Category = new Category
+                {
+                    Name = "Backend",
+                    Slug = "backend"
+                },
+                CreateDate = DateTime.Now,
+                Slug = "meu-artigo",
+                Summary = "Nesse artigo vamos conferir",
+                Title = "Meu artigo"
+            };
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
