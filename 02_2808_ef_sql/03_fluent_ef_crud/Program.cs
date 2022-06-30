54178e61ebe523d59804fd242836296e20db7589
Nardi <fjnf@outlook.com>
2022-06-30 02:21:22 -0300

    finished ef v3

--- 02_2808_ef_sql/03_fluent_ef_crud/Program.cs
+++ 02_2808_ef_sql/03_fluent_ef_crud/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Blog.Data;
 using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             using var context = new BlogDataContext();
+
+            var posts = context.Posts.ToListAsync();
+            var users = context.Users.ToListAsync();
+            // var posts = await context.Posts.ToListAsync();
+            // var users = await context.Users.ToListAsync();
+            Console.WriteLine("posts");
+            Console.WriteLine("users");
         }
     }
 }
