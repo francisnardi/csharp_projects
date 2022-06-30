ad474dd149a2a105aab24bda6d1cc14035d141e0
Nardi <fjnf@outlook.com>
2022-06-30 01:54:17 -0300

    finished ef v2

--- 02_2808_ef_sql/03_fluent_ef_crud/Program.cs
+++ 02_2808_ef_sql/03_fluent_ef_crud/Program.cs
@@ -10,11 +10,6 @@ namespace Blog
         static void Main(string[] args)
         {
             using var context = new BlogDataContext();
-            var user = context.Users.FirstOrDefault();
-            user.GitHub = "fran";         
-            context.Users.Update(user);
-            context.SaveChanges();
-            Console.WriteLine($"{user.GitHub}");
         }
     }
 }
