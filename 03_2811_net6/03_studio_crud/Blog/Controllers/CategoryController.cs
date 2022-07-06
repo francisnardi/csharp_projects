f54f8fe7d3499998e20a1abbbaa68fee2512d84b
Francisco Nardi <francisnardi@gmail.com>
2022-07-06 03:53:22 -0300

    api categories v2

--- 03_2811_net6/03_studio_crud/Blog/Controllers/CategoryController.cs
+++ 03_2811_net6/03_studio_crud/Blog/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Blog.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers
 {
@@ -7,10 +8,10 @@ namespace Blog.Controllers
     public class CategoryController : ControllerBase
     {
         [HttpGet("v1/categories")]
-        public IActionResult Get(
+        public async Task<IActionResult> GetAsync(
             [FromServices] BlogDataContext context)
         {
-            var categories = context.Categories.ToList();
+            var categories = await context.Categories.ToListAsync();
             return Ok(categories);
         }
     }
