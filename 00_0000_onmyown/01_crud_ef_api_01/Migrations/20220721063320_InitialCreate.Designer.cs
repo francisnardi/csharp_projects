fbcc1775b6c9505360c581c2908407620fb0793c
Nardi <fjnf@outlook.com>
2022-07-21 03:50:19 -0300

    starting crud 06 - model

--- 00_0000_onmyown/01_crud_ef_api_01/Migrations/20220721063320_InitialCreate.Designer.cs
+++ 00_0000_onmyown/01_crud_ef_api_01/Migrations/20220721063320_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Blog.Migrations
 {
     [DbContext(typeof(BlogDataContext))]
-    [Migration("20220721060904_InitialCreate")]
+    [Migration("20220721063320_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
